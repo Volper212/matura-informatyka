c33b728b48f83cc636a6a5b16d9e56a3fea761af
Igor Święs <swiesigor@gmail.com>
2024-05-16 13:46:32 +0200

    Optimise 3.3.py and 3.4.py

--- 2023/3.3.py
+++ 2023/3.3.py
@@ -2,6 +2,29 @@ digits = []
 with open("pi.txt") as file:
     for line in file.readlines():
         digits.append(int(line.strip()))
+lengths = [0]
+for i in range(1, len(digits)):
+    if digits[i] > digits[i - 1]:
+        lengths.append(max(1, lengths[i - 1] + 1))
+    elif digits[i] < digits[i - 1]:
+        lengths.append(min(-1, lengths[i - 1] - 1))
+    else:
+        lengths.append(0)
+count = 0
+for i in range(5, len(lengths)):
+    if lengths[i] >= 0:
+        continue
+    change = lengths[i]
+    if lengths[i + change] == 0 or change == -1:
+        change -= 1
+    if change <= -5:
+        continue
+    if lengths[i + change] - change + 1 < 6:
+        continue
+    count += 1
+print(count)
+
+""" another way:
 count = 0
 for i in range(len(digits) - 5):
     j = i
@@ -14,3 +37,4 @@ for i in range(len(digits) - 5):
         else:
             count += 1
 print(count)
+"""
--- 2023/3.4.py
+++ 2023/3.4.py
@@ -2,7 +2,35 @@ digits = []
 with open("pi.txt") as file:
     for line in file.readlines():
         digits.append(int(line.strip()))
+lengths = [0]
+for i in range(1, len(digits)):
+    if digits[i] > digits[i - 1]:
+        lengths.append(max(1, lengths[i - 1] + 1))
+    elif digits[i] < digits[i - 1]:
+        lengths.append(min(-1, lengths[i - 1] - 1))
+    else:
+        lengths.append(0)
 count = 0
+for j in range(len(lengths), 3, -1):
+    for i in range(j - 1, len(lengths)):
+        if lengths[i] >= 0:
+            continue
+        change = lengths[i]
+        if lengths[i + change] == 0 or change == -1:
+            change -= 1
+        if change <= -(j - 1):
+            continue
+        if lengths[i + change] - change + 1 < j:
+            continue
+        print(i - j + 1 + 1)
+        for k in range(i - j + 1, i + 1):
+            print(digits[k], end="")
+        break
+    else:
+        continue
+    break
+
+""" another way (less optimal):
 for length in range(len(digits), 3, -1):
     for i in range(len(digits) - (length - 1)):
         j = i
@@ -19,3 +47,4 @@ for length in range(len(digits), 3, -1):
     else:
         continue
     break
+"""
